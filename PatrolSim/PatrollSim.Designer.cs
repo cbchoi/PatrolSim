5c6c5edfd1799599447c9a3d5f6b0b529e63bade
Changbeom Choi <cbchoi@handong.edu>
2017-06-20 14:11:17 +0900

    Inital Commit

---
+++ PatrolSim/PatrollSim.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace PatrolSim
+{
+    partial class PatrolSim
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatrolSim));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newScenarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openScenarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveScenarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simulationControlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simulationStartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simulaitionPauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simulationResumeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simulationStopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simulationRatioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.realTimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ratio10XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ratio10XToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ratio20XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.analysisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timedAnaysisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chartSimulation = new Nevron.Chart.WinForm.NChartControl();
+            this.realLog = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.simLog = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.chartRealWorld = new Nevron.Chart.WinForm.NChartControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.simulationControlToolStripMenuItem,
+            this.simulationRatioToolStripMenuItem,
+            this.analysisToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(2022, 40);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newScenarioToolStripMenuItem,
+            this.openScenarioToolStripMenuItem,
+            this.saveScenarioToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 36);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newScenarioToolStripMenuItem
+            // 
+            this.newScenarioToolStripMenuItem.Name = "newScenarioToolStripMenuItem";
+            this.newScenarioToolStripMenuItem.Size = new System.Drawing.Size(271, 38);
+            this.newScenarioToolStripMenuItem.Text = "New Scenario";
+            // 
+            // openScenarioToolStripMenuItem
+            // 
+            this.openScenarioToolStripMenuItem.Name = "openScenarioToolStripMenuItem";
+            this.openScenarioToolStripMenuItem.Size = new System.Drawing.Size(271, 38);
+            this.openScenarioToolStripMenuItem.Text = "Open Scenario";
+            // 
+            // saveScenarioToolStripMenuItem
+            // 
+            this.saveScenarioToolStripMenuItem.Name = "saveScenarioToolStripMenuItem";
+            this.saveScenarioToolStripMenuItem.Size = new System.Drawing.Size(271, 38);
+            this.saveScenarioToolStripMenuItem.Text = "Save Scenario";
+            // 
+            // simulationControlToolStripMenuItem
+            // 
+            this.simulationControlToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.simulationStartToolStripMenuItem,
+            this.simulaitionPauseToolStripMenuItem,
+            this.simulationResumeToolStripMenuItem,
+            this.simulationStopToolStripMenuItem});
+            this.simulationControlToolStripMenuItem.Name = "simulationControlToolStripMenuItem";
+            this.simulationControlToolStripMenuItem.Size = new System.Drawing.Size(227, 36);
+            this.simulationControlToolStripMenuItem.Text = "Simulation Control";
+            // 
+            // simulationStartToolStripMenuItem
+            // 
+            this.simulationStartToolStripMenuItem.Name = "simulationStartToolStripMenuItem";
+            this.simulationStartToolStripMenuItem.Size = new System.Drawing.Size(319, 38);
+            this.simulationStartToolStripMenuItem.Text = "Simulation Start";
+            // 
+            // simulaitionPauseToolStripMenuItem
+            // 
+            this.simulaitionPauseToolStripMenuItem.Name = "simulaitionPauseToolStripMenuItem";
+            this.simulaitionPauseToolStripMenuItem.Size = new System.Drawing.Size(319, 38);
+            this.simulaitionPauseToolStripMenuItem.Text = "Simulaition Pause";
+            // 
+            // simulationResumeToolStripMenuItem
+            // 
+            this.simulationResumeToolStripMenuItem.Name = "simulationResumeToolStripMenuItem";
+            this.simulationResumeToolStripMenuItem.Size = new System.Drawing.Size(319, 38);
+            this.simulationResumeToolStripMenuItem.Text = "Simulation Resume";
+            // 
+            // simulationStopToolStripMenuItem
+            // 
+            this.simulationStopToolStripMenuItem.Name = "simulationStopToolStripMenuItem";
+            this.simulationStopToolStripMenuItem.Size = new System.Drawing.Size(319, 38);
+            this.simulationStopToolStripMenuItem.Text = "Simulation Stop";
+            // 
+            // simulationRatioToolStripMenuItem
+            // 
+            this.simulationRatioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.realTimeToolStripMenuItem,
+            this.ratio10XToolStripMenuItem,
+            this.ratio10XToolStripMenuItem1,
+            this.ratio20XToolStripMenuItem});
+            this.simulationRatioToolStripMenuItem.Name = "simulationRatioToolStripMenuItem";
+            this.simulationRatioToolStripMenuItem.Size = new System.Drawing.Size(202, 36);
+            this.simulationRatioToolStripMenuItem.Text = "Simulation Ratio";
+            // 
+            // realTimeToolStripMenuItem
+            // 
+            this.realTimeToolStripMenuItem.Name = "realTimeToolStripMenuItem";
+            this.realTimeToolStripMenuItem.Size = new System.Drawing.Size(218, 38);
+            this.realTimeToolStripMenuItem.Text = "Real Time";
+            // 
+            // ratio10XToolStripMenuItem
+            // 
+            this.ratio10XToolStripMenuItem.Name = "ratio10XToolStripMenuItem";
+            this.ratio10XToolStripMenuItem.Size = new System.Drawing.Size(218, 38);
+            this.ratio10XToolStripMenuItem.Text = "Ratio:0.5X";
+            this.ratio10XToolStripMenuItem.Click += new System.EventHandler(this.ratio10XToolStripMenuItem_Click);
+            // 
+            // ratio10XToolStripMenuItem1
+            // 
+            this.ratio10XToolStripMenuItem1.Name = "ratio10XToolStripMenuItem1";
+            this.ratio10XToolStripMenuItem1.Size = new System.Drawing.Size(218, 38);
+            this.ratio10XToolStripMenuItem1.Text = "Ratio:1.0X";
+            // 
+            // ratio20XToolStripMenuItem
+            // 
+            this.ratio20XToolStripMenuItem.Name = "ratio20XToolStripMenuItem";
+            this.ratio20XToolStripMenuItem.Size = new System.Drawing.Size(218, 38);
+            this.ratio20XToolStripMenuItem.Text = "Ratio:2.0X";
+            // 
+            // analysisToolStripMenuItem
+            // 
+            this.analysisToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.timedAnaysisToolStripMenuItem});
+            this.analysisToolStripMenuItem.Name = "analysisToolStripMenuItem";
+            this.analysisToolStripMenuItem.Size = new System.Drawing.Size(112, 36);
+            this.analysisToolStripMenuItem.Text = "Analysis";
+            // 
+            // timedAnaysisToolStripMenuItem
+            // 
+            this.timedAnaysisToolStripMenuItem.Name = "timedAnaysisToolStripMenuItem";
+            this.timedAnaysisToolStripMenuItem.Size = new System.Drawing.Size(267, 38);
+            this.timedAnaysisToolStripMenuItem.Text = "Timed Anaysis";
+            // 
+            // chartSimulation
+            // 
+            this.chartSimulation.AutoRefresh = false;
+            this.chartSimulation.BackColor = System.Drawing.SystemColors.Control;
+            this.chartSimulation.InputKeys = new System.Windows.Forms.Keys[0];
+            this.chartSimulation.Location = new System.Drawing.Point(41, 42);
+            this.chartSimulation.Name = "chartSimulation";
+            this.chartSimulation.Size = new System.Drawing.Size(900, 900);
+            this.chartSimulation.State = ((Nevron.Chart.WinForm.NState)(resources.GetObject("chartSimulation.State")));
+            this.chartSimulation.TabIndex = 1;
+            this.chartSimulation.Text = "SimulationMap";
+            // 
+            // realLog
+            // 
+            this.realLog.FormattingEnabled = true;
+            this.realLog.HorizontalScrollbar = true;
+            this.realLog.ItemHeight = 25;
+            this.realLog.Location = new System.Drawing.Point(17, 45);
+            this.realLog.Name = "realLog";
+            this.realLog.Size = new System.Drawing.Size(956, 329);
+            this.realLog.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.realLog);
+            this.groupBox1.Location = new System.Drawing.Point(1012, 1040);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(987, 391);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Real World Log";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.simLog);
+            this.groupBox2.Location = new System.Drawing.Point(17, 1040);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(987, 391);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Simulation Log";
+            // 
+            // simLog
+            // 
+            this.simLog.FormattingEnabled = true;
+            this.simLog.HorizontalScrollbar = true;
+            this.simLog.ItemHeight = 25;
+            this.simLog.Location = new System.Drawing.Point(17, 45);
+            this.simLog.Name = "simLog";
+            this.simLog.Size = new System.Drawing.Size(956, 329);
+            this.simLog.TabIndex = 4;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.chartSimulation);
+            this.groupBox3.Location = new System.Drawing.Point(17, 43);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(987, 975);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Simulation Map";
+            this.groupBox3.UseCompatibleTextRendering = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.chartRealWorld);
+            this.groupBox4.Location = new System.Drawing.Point(1012, 43);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(987, 975);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Real World Map";
+            this.groupBox4.UseCompatibleTextRendering = true;
+            // 
+            // chartRealWorld
+            // 
+            this.chartRealWorld.AutoRefresh = false;
+            this.chartRealWorld.BackColor = System.Drawing.SystemColors.Control;
+            this.chartRealWorld.InputKeys = new System.Windows.Forms.Keys[0];
+            this.chartRealWorld.Location = new System.Drawing.Point(41, 42);
+            this.chartRealWorld.Name = "chartRealWorld";
+            this.chartRealWorld.Size = new System.Drawing.Size(900, 900);
+            this.chartRealWorld.State = ((Nevron.Chart.WinForm.NState)(resources.GetObject("chartRealWorld.State")));
+            this.chartRealWorld.TabIndex = 1;
+            this.chartRealWorld.Text = "SimulationMap";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(948, 1437);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 32);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PatrolSim
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(2022, 1481);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "PatrolSim";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "PatrolSim";
+            this.Load += new System.EventHandler(this.PatrolSim_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newScenarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openScenarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveScenarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simulationControlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simulationStartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simulaitionPauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simulationResumeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simulationStopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simulationRatioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem analysisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem realTimeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ratio10XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ratio10XToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ratio20XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timedAnaysisToolStripMenuItem;
+        private Nevron.Chart.WinForm.NChartControl chartSimulation;
+        private System.Windows.Forms.ListBox realLog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox simLog;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private Nevron.Chart.WinForm.NChartControl chartRealWorld;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PatrolSim/PatrollSim.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Nevron.Chart;
+using Nevron.Chart.WinForm;
+using Nevron.Dom;
+using Nevron.GraphicsCore;
+
+namespace PatrolSim
+{
+    public partial class PatrolSim : Form
+    {
+        private readonly int _gridSizeX = 100;
+        private readonly int _gridSizeY = 100;
+        private readonly Color[] _colorTable;
+
+        private double[][] matrixSimulation;
+        private double[][] matrixRealWorld;
+
+        public PatrolSim()
+        {
+            InitializeComponent();
+
+            _colorTable = new Color[21];
+
+            for (int i = 0; i <= 20; i++)
+            {
+                _colorTable[i] = InterpolateColors(Color.DeepSkyBlue, Color.Red, i / 20.0F);
+            }
+
+            InitMap(chartSimulation);
+            InitMap(chartRealWorld);
+
+            matrixRealWorld = new double[_gridSizeY][];
+            matrixSimulation = new double[_gridSizeX][];
+
+            for (int i = 0; i < _gridSizeY; i++)
+            {
+                matrixRealWorld[i] = new double[_gridSizeX];
+                matrixSimulation[i] = new double[_gridSizeX];
+            }
+        }
+
+        public static Color InterpolateColors(Color color1, Color color2, float factor)
+        {
+            int num1 = ((int)color1.R);
+            int num2 = ((int)color1.G);
+            int num3 = ((int)color1.B);
+
+            int num4 = ((int)color2.R);
+            int num5 = ((int)color2.G);
+            int num6 = ((int)color2.B);
+
+            byte num7 = (byte)((((float)num1) + (((float)(num4 - num1)) * factor)));
+            byte num8 = (byte)((((float)num2) + (((float)(num5 - num2)) * factor)));
+            byte num9 = (byte)((((float)num3) + (((float)(num6 - num3)) * factor)));
+
+            return Color.FromArgb(num7, num8, num9);
+        }
+
+        private void InitMap(NChartControl nChartControl)
+        {
+            // configure the chart
+            NCartesianChart chart = (NCartesianChart)nChartControl.Charts[0];
+            chart.Projection.SetPredefinedProjection(PredefinedProjection.OrthogonalTop);
+            chart.BoundsMode = BoundsMode.Fit;
+            chart.Enable3D = true;
+            chart.Fit3DAxisContent = false;
+
+            // No Legend & No Update
+            nChartControl.Legends.Clear();
+            nChartControl.ServiceManager.LegendUpdateService.UpdateAutoLegends();
+            nChartControl.ServiceManager.LegendUpdateService.Stop();
+
+            chart.Width = _gridSizeX;
+            chart.Height = 20;
+            chart.Depth = _gridSizeY;
+
+            // Wall Setup
+            NChartWall backWall = chart.Wall(ChartWallType.Back);
+            backWall.Visible = false;
+
+            NChartWall leftWall = chart.Wall(ChartWallType.Left);
+            leftWall.Visible = false;
+
+            for (int i = 0; i < _gridSizeY; i++)
+            {
+                // add the first line
+                NBarSeries bar = new NBarSeries();
+                chart.Series.Add(bar);
+
+                bar.WidthPercent = 100.0f;
+                bar.DepthPercent = 100.0f;
+
+                bar.EnableDepthSort = false;
+                bar.DataLabelStyle.Visible = false;
+
+                //bar.Values.ValueFormatter = new NNumericValueFormatter("0.0");
+                bar.Values.EmptyDataPoints.ValueMode = EmptyDataPointsValueMode.Skip;
+
+                //bar.Values.Clear();
+                bar.FillStyles.StorageType = IndexedStorageType.Array;
+                bar.DataPointOriginIndex = 0;
+
+                // turn off bar border to improve performance
+                bar.BorderStyle.Width = new NLength(0);
+            }
+        }
+
+        private void UpdateMap(NChartControl nChartControl, int gridY, double[] valList)
+        {
+            NCartesianChart chart = (NCartesianChart)nChartControl.Charts[0];
+            // fill grid to bars
+            //for (int i = 0; i < 100; i++)
+            {
+                NBarSeries bar = chart.Series[gridY] as NBarSeries;
+                double[] barValues = valList;
+                int barValueCount = barValues.Length;
+
+                if (bar.Values.Count == 0)
+                {
+                    bar.Values.AddRange(barValues);
+                }
+                else
+                {
+                    bar.Values.SetRange(0, barValues);
+                }
+
+                int fillStyleCount = bar.FillStyles.Count;
+
+                for (int j = 0; j < barValueCount; j++)
+                {
+                    if (j >= fillStyleCount)
+                    {
+                        bar.FillStyles[j] = new NColorFillStyle(_colorTable[(int)barValues[j]]);
+                    }
+                    else
+                    {
+                        ((NColorFillStyle)bar.FillStyles[j]).Color = _colorTable[(int)barValues[j]];
+                    }
+                }
+            }
+
+            nChartControl.Refresh();
+        }
+
+        private Tuple<int, int> UpdateMatrix(NChartControl nChartControl, double[][] matrix)
+        {
+            Random rnd = new Random();
+            int value = (int)Math.Max(1, rnd.NextDouble() * 20);
+            int x = rnd.Next(0, 99);
+            int y = rnd.Next(0, 99);
+
+            matrix[y][x] = value;
+
+            NCartesianChart chart = (NCartesianChart)nChartControl.Charts[0];
+            for (int i = 0; i < _gridSizeY; i++)
+            {
+                NBarSeries bar = chart.Series[i] as NBarSeries;
+                double[] barValues = matrix[i];
+                int barValueCount = barValues.Length;
+
+                if (bar.Values.Count == 0)
+                {
+                    bar.Values.AddRange(barValues);
+                }
+                else
+                {
+                    bar.Values.SetRange(0, barValues);
+                }
+
+                int fillStyleCount = bar.FillStyles.Count;
+
+                for (int j = 0; j < barValueCount; j++)
+                {
+                    if (j >= fillStyleCount)
+                    {
+                        bar.FillStyles[j] = new NColorFillStyle(_colorTable[(int)barValues[j]]);
+                    }
+                    else
+                    {
+                        ((NColorFillStyle)bar.FillStyles[j]).Color = _colorTable[(int)barValues[j]];
+                    }
+                }
+            }
+            nChartControl.Refresh();
+
+            return Tuple.Create(x, y);
+        }
+
+        private void UpdateLog(ListBox lb, String objID, String eventLog, int x, int y)
+        {
+            String strLog = String.Format("{0:u} {1} {2} ({3}, {4})", DateTime.Now, objID, eventLog, x, y);
+            lb.Items.Add(strLog);
+            lb.SelectedIndex = lb.Items.Count - 1;
+
+        }
+
+        private void ratio10XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PatrolSim_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tuple<int, int> coordinate = UpdateMatrix(chartSimulation, matrixSimulation);
+            //UpdateMap(chartSimulation, coordinate.Item2, matrixSimulation[coordinate.Item2]);
+            UpdateLog(simLog, "agent1", "Moved", coordinate.Item1, coordinate.Item2);
+
+            //value = (int)Math.Max(1, rnd.NextDouble() * 20);
+            //x = rnd.Next(0, 99);
+            //y = rnd.Next(0, 99);
+
+            coordinate = UpdateMatrix(chartRealWorld,matrixRealWorld);
+            //UpdateMap(chartRealWorld, coordinate.Item2, matrixRealWorld[coordinate.Item2]);
+            UpdateLog(realLog, "ship1", "Moved", coordinate.Item1, coordinate.Item2);
+        }
+    }
+}
