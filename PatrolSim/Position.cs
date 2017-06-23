c4f161700de72c624ce08f0da3e43ccb0c38bfae
Changbeom Choi <cbchoi@handong.edu>
2017-06-23 20:34:25 +0900

    Waypoint Management

--- PatrolSim/Position.cs
+++ PatrolSim/Position.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace PatrolSim
 {
-    public class Waypoint
+    public class Position
     {
         private int _x;
         private int _y;
@@ -14,7 +14,7 @@ namespace PatrolSim
         public int X { get { return _x; } }
         public int Y { get { return _y; } }
 
-        public Waypoint(int x, int y, int z)
+        public Position(int x, int y, int z)
         {
             _x = x;
             _y = y;
